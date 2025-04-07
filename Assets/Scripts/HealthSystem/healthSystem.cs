9ddfd0180d091df863e8626c560796d4cff0b2f5
WishedClover48 <matuleon2001@gmail.com>
2025-04-07 19:24:02 -0300

    Modified HealthSystem
    Upgraded HealthSystem to fit better with the UML and design ideas. Also moved the InputActionMap script into the scripts folder.

--- Assets/Scripts/HealthSystem/healthSystem.cs
+++ Assets/Scripts/HealthSystem/healthSystem.cs
@@ -1,85 +1,132 @@
+using System;
 using UnityEngine;
-
-/*
-    Cualquier cosa si no se entiende, esta mal, tiene un error, avisenme.
-    - Maximo
-
-   HV = HealthValue
-
-    */
-
-public class healthSystem 
+public class HealthSystem
 {
-    private float maxHealth;
-    private float currentHealth;
-    private float savedCurrentHealth;
-    private void Awake()
+    private float _maxHealth;
+    private float _currentHealth;
+
+    public float maxHealth
     {
-        currentHealth = maxHealth;
-        savedCurrentHealth = currentHealth;
+        get => _maxHealth;
+        private set => _maxHealth = Mathf.Max(0, value);
     }
-    public float getMaxHealth()
+    public float currentHealth
+    {
+        get => _currentHealth;
+        private set => _currentHealth = Mathf.Clamp(value, 0, maxHealth);
+    }
+
+    #region EVENTS
+
+    /// <summary>
+    /// Event triggered when the entity dies (health reaches 0).
+    /// </summary>
+    public event Action OnDead;
+
+    /// <summary>
+    /// Event triggered when the entity is healed.
+    /// The float parameter indicates the amount healed.
+    /// </summary>
+    public event Action<float> OnHeal;
+
+    /// <summary>
+    /// Event triggered when the entity takes damage.
+    /// The float parameter indicates the amount of damage received.
+    /// </summary>
+    public event Action<float> OnDamage;
+
+    #endregion
+
+    /// <summary>
+    /// Initializes a new instance of the HealthSystem with a specified maximum health.
+    /// </summary>
+    /// <param name="MaxHealth">The maximum health value to initialize with.</param>
+    public HealthSystem(float MaxHealth)
+    {
+        maxHealth = MaxHealth;
+        currentHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Increases current health by a specified amount and invokes the OnHeal event.
+    /// </summary>
+    /// <param name="healingAmount">The amount of health to restore.</param>
+    public void Heal(float healingAmount)
+    {
+        currentHealth += healingAmount;
+        OnHeal?.Invoke(healingAmount);
+    }
+
+    /// <summary>
+    /// Returns the maximum health value.
+    /// </summary>
+    public float GetMaxHealth()
     {
         return maxHealth;
     }
 
-    public float getCurrentHealth()
+    /// <summary>
+    /// Returns the current health value.
+    /// </summary>
+    public float GetCurrentHealth()
     {
         return currentHealth;
     }
 
-    public float getCurrentHealthPercentage()
+    /// <summary>
+    /// Returns the current health as a percentage of max health.
+    /// </summary>
+    public float GetCurrentHealthPercentage()
     {
-        if (currentHealth <= 0)
-        {
-            return 0;
-        }
-
         return (currentHealth / maxHealth) * 100f;
     }
-    public void heal(float HV)
+
+    /// <summary>
+    /// Fully restores health to the maximum value and invokes the OnHeal event.
+    /// </summary>
+    public void FullHeal()
     {
-        currentHealth += HV;
-    }
-    public void fullHeal()
-    {
-        currentHealth = savedCurrentHealth;
+        currentHealth = maxHealth;
+        OnHeal?.Invoke(maxHealth);
     }
 
-    public void damage(float HV)
+    /// <summary>
+    /// Reduces current health by a specified amount and invokes the OnDamage event.
+    /// If health falls to zero or below, triggers the Kill method.
+    /// </summary>
+    /// <param name="damageAmount">The amount of damage to apply.</param>
+    public void TakeDamage(float damageAmount)
     {
-        currentHealth -= HV;
-
+        currentHealth -= damageAmount;
+        OnDamage?.Invoke(damageAmount);
         if (currentHealth <= 0)
         {
-            kill();
+            Kill();
         }
     }
-    public void kill()
+
+    /// <summary>
+    /// Forces the entity to take lethal damage equal to its max health.
+    /// Triggers the OnDead event if health drops to zero or below.
+    /// </summary>
+    public void Kill()
     {
-        //isAlive = false;
-        //Dead entity logic.
+        currentHealth -= maxHealth;
+        if (currentHealth <= 0)
+        {
+            OnDead?.Invoke();
+        }
+        else
+        {
+            Debug.LogError("did not die!!!");
+        }
     }
 
-    public bool isAlive()
+    /// <summary>
+    /// Checks whether the entity is still alive (health greater than zero).
+    /// </summary>
+    public bool IsAlive()
     {
-       //While entity is alive....
-       return currentHealth > 0;
+        return currentHealth > 0;
     }
-
-    public void onDeadEvent()
-    {
-        //update when gameplay is defined
-    }
-
-    public void onHealEvent()
-    {
-        //update when gameplay is defined
-    }
-
-    public void onDamageEvent()
-    {
-        //update when gameplay is defined
-    }
-
 }
