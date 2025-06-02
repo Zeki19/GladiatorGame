408f3f60aa3bad6e453f1d64652addafa4d146ad
WishedClover48 <matuleon2001@gmail.com>
2025-06-02 13:05:22 -0300

    Added flocking
    Added the scripts for the logic of Avoidance, Alignment and Cohesion.

---
+++ Assets/Scripts/Enemies/Steerings/FlockingBehaviours/Cohesion.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cohesion : MonoBehaviour, IFlocking
+{
+    [SerializeField] public float multiplier = 1;
+    public Vector3 GetDir(List<IBoid> boids, IBoid self)
+    {
+        Vector3 cohesion = Vector3.zero;
+        Vector3 center = Vector3.zero;
+
+        for (int i = 0; i< boids.Count; i++)
+        {
+            center += boids[i].Position;
+        }
+
+        if (boids.Count > 0) 
+        { 
+            center /= boids.Count;
+            cohesion = center - self.Position;
+        }
+
+        return cohesion * multiplier;
+    }
+}
