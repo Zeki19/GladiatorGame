76271002cbfbae46d916321ee1a793f69c3dadb3
MaximoB470 <157617812+MaximoB470@users.noreply.github.com>
2025-06-15 12:04:28 -0300

    adds FinishedPlayer HB And ChargeBar

---
+++ Assets/Scripts/Player/PlayerChargeBar.cs
@@ -0,0 +1,27 @@
+using Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerChargeBar : MonoBehaviour
+{
+    [SerializeField] private Slider chargeSlider;
+    private PlayerWeaponController _weaponController;
+
+    private void Start()
+    {
+        _weaponController = ServiceLocator.Instance.GetService<PlayerWeaponController>();
+
+        if (chargeSlider == null)
+            chargeSlider = GetComponent<Slider>();
+
+        chargeSlider.value = 0f;
+    }
+
+    private void Update()
+    {
+        if (_weaponController != null && chargeSlider != null)
+        {
+            chargeSlider.value = _weaponController.CheckWeaponChargePercent() / 100f;
+        }
+    }
+}
