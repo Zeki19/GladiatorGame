4b5b263d97188e3b0f49e6b046e453505278fea9
MaximoB470 <157617812+MaximoB470@users.noreply.github.com>
2025-07-27 13:42:23 -0300

    changes Boss bar UI WIP

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -3,9 +3,15 @@ using Enemies;
 using Entities;
 using UnityEngine;
 
+
 public class EnemyManager : EntityManager
 {
     [SerializeField] private int maxLife;
+
+    [Header("Boss UI Reference")]
+    [SerializeField] private BossHealthBarUI bossHealthBarUI;
+
+    private int currentPhase = 0;
 
     private void Awake()
     {
@@ -17,14 +23,43 @@ public class EnemyManager : EntityManager
 
     private void Start()
     {
-        ServiceLocator.Instance.GetService<EnemiesManager>().RegisterEnemy(gameObject,this);
+        ServiceLocator.Instance.GetService<EnemiesManager>().RegisterEnemy(gameObject, this);
+        UpdateBossPhaseUI();
     }
+
     private void PrintHealth(float ignore)
     {
         Debug.Log(HealthSystem.currentHealth);
     }
-    public Vector2 GetEnemyPosition() 
-    { 
+
+    public Vector2 GetEnemyPosition()
+    {
         return gameObject.transform.position;
+    }
+
+    public int CurrentPhase
+    {
+        get => currentPhase;
+        set
+        {
+            if (currentPhase != value)
+            {
+                currentPhase = value;
+                UpdateBossPhaseUI();
+            }
+        }
+    }
+
+    private void UpdateBossPhaseUI()
+    {
+        if (bossHealthBarUI != null)
+        {
+            bossHealthBarUI.SetBossPhase(currentPhase);
+        }
+    }
+
+    public void AdvancePhase()
+    {
+        CurrentPhase++;
     }
 }
