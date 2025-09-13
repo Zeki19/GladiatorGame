ba8e542d0c1c304eff19d87a003f54a8d61905a3
EgiglioUade <146669271+Zeki19@users.noreply.github.com>
2025-09-13 19:40:40 -0300

    Deletead name from FoodSO

--- Assets/Scripts/Food/SOFood.cs
+++ Assets/Scripts/Food/SOFood.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FoodType",menuName = "New Food/Food")]
 public class SOFood : ScriptableObject
 {
-    public string foodType;
     public Sprite foodSprite;
     public float healingAmount;
 }
