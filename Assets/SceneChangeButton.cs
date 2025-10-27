af3c5f9e1f327e351549a7f4aed9b689f4dd239b
EgiglioUade <146669271+Zeki19@users.noreply.github.com>
2025-10-27 16:33:49 -0300

    Changed Pause InGame

--- Assets/SceneChangeButton.cs
+++ Assets/SceneChangeButton.cs
@@ -4,4 +4,5 @@ public class SceneChangeButton : MonoBehaviour
 {
     public void ChangeScene(string sceneName) => SceneChanger.Instance.ChangeScene(sceneName);
     public void ChangeScene(int sceneId) => SceneChanger.Instance.ChangeScene(sceneId);
+    public void Quit() => Application.Quit();
 }
