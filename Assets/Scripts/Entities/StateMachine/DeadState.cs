974b1df7a28e5e00398f5f90f971828960f66a07
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2025-11-04 18:53:36 -0300

    dead and weapon change

--- Assets/Scripts/Entities/StateMachine/DeadState.cs
+++ Assets/Scripts/Entities/StateMachine/DeadState.cs
@@ -10,6 +10,10 @@ namespace Entities.StateMachine
             _animate.PlayStateAnimation(StateEnum.Death);
             _agent._NVagent.velocity = Vector3.zero;
             _agent._NVagent.enabled = false;
+            foreach (var collider2D in _agent._NVagent.gameObject.GetComponents<Collider2D>())
+            {
+                collider2D.enabled = false;
+            }
         }
     }
 }
--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -11,26 +11,31 @@ namespace Weapons
     public class WeaponManager : MonoBehaviour
     {
         #region ForTesting
-        
-            private Factory<Weapon, SoWeapon> _factory;
+
+        private Factory<Weapon, SoWeapon> _factory;
 
         #endregion
 
         [SerializeField] private Transform inactiveWeapon;
         private WeaponPool _pool;
-        
-        [Header("Weapons list")]
-        [SerializeField] private List<SoWeapon>  weapons;
+
+        [Header("Weapons list")] [SerializeField]
+        private List<SoWeapon> weapons;
+
         private Dictionary<GameObject, Weapon> _droppedWeapons;
-        
-        [Header("SpawnPoints list")]
-        [SerializeField] private List<Vector3> spawnPoints;
+
+        [Header("SpawnPoints list")] [SerializeField]
+        private List<Vector3> spawnPoints;
+
         [SerializeField] private List<Vector3> usedSpawnPoint;
-        
-        [Header("Respawn config")]
-        [SerializeField] private Vector2 RespawnTimeRange;
+
+        [Header("Respawn config")] [SerializeField]
+        private Vector2 RespawnTimeRange;
+
         [SerializeField] private Vector2Int RespawnWeaponQuantity;
-        
+        [SerializeField] private int MinimumAmountOfWeaponsInTheGround;
+
+
         private void Awake()
         {
             _factory = new Factory<Weapon, SoWeapon>();
@@ -38,6 +43,7 @@ namespace Weapons
             _pool = new WeaponPool(_factory.Create);
             ServiceLocator.Instance.RegisterService(this);
         }
+
         private void Start()
         {
             CrateWeaponInRandomPoint();
@@ -47,38 +53,40 @@ namespace Weapons
         IEnumerator RespawnWeapon()
         {
             yield return new WaitForSeconds(Random.Range(RespawnTimeRange.x, RespawnTimeRange.y));
-            for (int i = 0; i < Random.Range(RespawnWeaponQuantity.x,RespawnWeaponQuantity.y+1); i++)
+            for (int i = 0; i < Random.Range(RespawnWeaponQuantity.x, RespawnWeaponQuantity.y + 1); i++)
             {
                 CrateWeaponInRandomPoint();
             }
         }
-        
+
         private Weapon CreateWeapon(SoWeapon weaponConfig)
         {
             var newWeapon = _pool.Get(weaponConfig);
-            _droppedWeapons.Add(newWeapon.WeaponGameObject,newWeapon);
+            _droppedWeapons.Add(newWeapon.WeaponGameObject, newWeapon);
             newWeapon.WeaponGameObject.transform.parent = transform;
             newWeapon.WeaponGameObject.SetActive(true);
             return newWeapon;
         }
+
         public Weapon RequestWeapon(SoWeapon weaponConfig)
         {
-            Weapon weapon =CreateWeapon(weaponConfig);
+            Weapon weapon = CreateWeapon(weaponConfig);
             _droppedWeapons.Remove(weapon.WeaponGameObject);
             return weapon;
         }
-        
+
         [ContextMenu("Spawn Weapon")]
         private void CrateWeaponInRandomPoint()
         {
-            Weapon weapon = CreateWeapon(weapons[Random.Range(0,weapons.Count)]);
+            Weapon weapon = CreateWeapon(weapons[Random.Range(0, weapons.Count)]);
 
             var point = GetRandomSpawnPoint();
             weapon.WeaponGameObject.transform.position = point;
-            
+
             float randomZ = Random.Range(0f, 360f);
             weapon.WeaponGameObject.transform.rotation = Quaternion.Euler(0f, 0f, randomZ);
         }
+
         private Vector3 GetRandomSpawnPoint()
         {
             if (spawnPoints.Count == 0)
@@ -86,23 +94,24 @@ namespace Weapons
                 spawnPoints = new List<Vector3>(usedSpawnPoint);
                 usedSpawnPoint.Clear();
             }
-            
+
             var index = Random.Range(0, spawnPoints.Count);
             var returnValue = spawnPoints[index];
             spawnPoints.RemoveAt(index);
             usedSpawnPoint.Add(returnValue);
-            
+
             return returnValue;
         }
-        
+
         public void DestroyWeapon(Weapon weapon)
         {
             _pool.Release(weapon);
             weapon.WeaponGameObject.transform.parent = inactiveWeapon;
             weapon.WeaponGameObject.SetActive(false);
-            if (_droppedWeapons.Count == 0)
+            if (_droppedWeapons.Count == MinimumAmountOfWeaponsInTheGround)
                 StartCoroutine(RespawnWeapon());
         }
+
         public Weapon PickUpWeaponInRange(Vector3 pos, float range)
         {
             foreach (var weapon in _droppedWeapons)
@@ -111,24 +120,26 @@ namespace Weapons
                     Weapon ToGive;
                     ToGive = weapon.Value;
                     _droppedWeapons.Remove(weapon.Key);
-                    Debug.Log(_droppedWeapons.Count-1);
+                    Debug.Log(_droppedWeapons.Count - 1);
                     return ToGive;
                 }
+
             return default;
         }
+
         public void CatchDroppedWeapon(Weapon weapon)
         {
-            _droppedWeapons.Add(weapon.WeaponGameObject,weapon);
+            _droppedWeapons.Add(weapon.WeaponGameObject, weapon);
             weapon.WeaponGameObject.GetComponent<Collider2D>().enabled = true;
             weapon.WeaponGameObject.transform.parent = transform;
         }
+
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.magenta;
             for (int i = 0; i < spawnPoints.Count; i++)
             {
-                Gizmos.DrawSphere(spawnPoints[i],0.5f);
-                
+                Gizmos.DrawSphere(spawnPoints[i], 0.5f);
             }
         }
     }
